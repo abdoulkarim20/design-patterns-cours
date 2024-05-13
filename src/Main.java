be4fcabbb3c2d750e39623b095dd61324c679907
Abdoul Karim DIALLO <akdiallo@gainde2000.sn>
2024-05-13 00:00:53 +0000

    mise en place d'une petit architecture en respectant le design pattern-abstract-factory

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,7 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        System.out.println("Bonjour chers membre");
+        System.out.println("Bienvenue au cours dédié spécifiquement aux design patterns");
+        System.out.println("By Abdoul Karim DIALLO");
     }
 }
